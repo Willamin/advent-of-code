20a873d227d32fae9fed09a746392ded9eaa095a
Will Lewis <will@wflewis.com>
2017-12-13 11:45:05 -0500

    Fix tests for day 5

--- day05/spec/maze_spec.cr
+++ day05/spec/maze_spec.cr
@@ -3,10 +3,10 @@ require "../src/maze"
 
 describe Maze do
   it "works for part1" do
-    Maze.part1("input").should eq("output")
+    Maze.part1("0\n3\n0\n1\n-3").should eq("5")
   end
 
   it "works for part2" do
-    Maze.part2("input").should eq("output")
+    Maze.part2("0\n3\n0\n1\n-3").should eq("10")
   end
 end
